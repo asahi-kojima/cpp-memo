650c3a341542681400549b4c876a69884cee9387
asahi-kojima-mac <none>
2024-12-29 00:03:54 +0900

    別パターンを追加

--- link-memo/static-link/recipe.sh
+++ link-memo/static-link/recipe.sh
@@ -1,10 +1,15 @@
 #! /bin/zsh
 
+rm *.a *.o
 
 g++ -std=c++11 -c util.cpp
 
 ar rc libtest.a util.o
 
-g++ -std=c++11 main.cpp libtest.a -o main
+g++ -std=c++11 main.cpp libtest.a -o main.out
+
+#こっちも動く
+#ar rc libtest.a util.o
+#g++ -std=c++11 main.cpp -L. -ltest -o main.out
 
 ./main.out
